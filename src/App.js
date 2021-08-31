85f6bc07f62472b908978f7fcb24c156dda10ab0
jr1412594 <jr.medina1412@gmail.com>
2021-08-30 21:57:59 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default class App extends Component {
 
   state = {
     tvShows: [],
+    count: 0
   }
   
   componentDidMount() {
@@ -18,11 +19,19 @@ export default class App extends Component {
     .then(result => this.setState({tvShows: result}))
   }
 
+  handleClick = () => {
+    this.setState(prev => ({count: prev.count + 1}))
+  }
+
   render(){
 
       return (
       <div className='App'>
         <h1>hello</h1>
+        <div>
+            <button onClick={this.handleClick}>Press Me</button>
+            <h1>{this.state.count}</h1>
+        </div>
         <TvShowContainer tvShows={this.state.tvShows}/>
       </div>
     )
--- src/components/TvShowContainer.js
+++ src/components/TvShowContainer.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TvShowCard from './TvShowCard';
 
 export default function TvShowContainer({ tvShows }) {
 
+    const [count, setCount] = useState(0)
+
     const displayTvShows =() => tvShows.map(tvShow => (
         <TvShowCard tvShow={tvShow} key={tvShow.id}/>
     ))
 
+    const updateCount = () => {
+        setCount(count + 1)
+    } 
+
 
     return (
         <div className='tvshow-container'>
+            <button onClick={updateCount}>Press Me Too!</button>
+            {count}
             {displayTvShows()}
         </div>
     )
